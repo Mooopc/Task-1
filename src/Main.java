40fe775bdd19d8b934a8527da88d83090e0c4166
Roman Boytsov <domovon@outlook.com>
2023-05-18 13:21:49 +0300

    Main.java changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ticket_price = 10_001;
+        int rubels_for_one_bonus_mile = 20;
+        int total_bonus_miles = ticket_price / rubels_for_one_bonus_mile;
+        System.out.printf("Количество бонусных миль за билет: " + total_bonus_miles);
     }
 }
