1f9296d81aad0a7f9334f1124f97bd10d3ff45dc
Roman Boytsov <domovon@outlook.com>
2023-05-22 20:39:25 +0300

    CamelCasing

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 10_001;
-        int rubels_for_one_bonus_mile = 20;
-        int total_bonus_miles = ticket_price / rubels_for_one_bonus_mile;
-        System.out.printf("Количество бонусных миль за билет: " + total_bonus_miles);
+        int ticketPrice = 10_001;
+        int rubelsForOneBonusMile = 20;
+        int totalBonusMiles = ticketPrice / rubelsForOneBonusMile;
+        System.out.printf("Количество бонусных миль за билет: " + totalBonusMiles);
     }
 }
